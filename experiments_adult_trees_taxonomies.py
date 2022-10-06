ce0feeef56e3f93a1b0f4e849520f54d9ac8107c
elianap <eliana.p1993@gmail.com>
2022-10-06 18:33:58 +0200

    simplify

--- experiments_adult_trees_taxonomies.py
+++ experiments_adult_trees_taxonomies.py
@@ -7,29 +7,24 @@ import numpy as np
 
 pd.set_option("display.max_colwidth", None)
 
-
 def run_adult_experiments_trees_taxonomies(
     name_output_dir="output",
-    type_experiments=["one_at_time", "all_attributes"],
+    type_experiment="one_at_time",
     type_criterion="divergence_criterion",
     min_support_tree=0.1,
     min_sup_divergences=[0.01, 0.02, 0.03, 0.04, 0.05, 0.1, 0.15, 0.2],
-    metrics=["d_outcome"],
-    show_fig=False,
+    metric="d_outcome",
     verbose=False,
-    no_compute_divergence=False,
-    minimal_gain=0,
     ouput_folder_dir=".",
+    minimal_gain = 0, 
 ):
-    out = {metric: {} for metric in metrics}
+
+
     info_list = ["FP", "max"]
     type_gens = ["with_gen", "without_gen"]
-    for metric in metrics:
-        out[metric] = {k: {} for k in info_list}
-        for i in info_list:
-            out[metric][i] = {k: {} for k in type_experiments}
-            for type_experiment in type_experiments:
-                out[metric][i][type_experiment] = {k: {} for k in type_gens}
+    out = {k: {} for k in info_list}
+    for i in info_list:
+        out[i] = {k: {} for k in type_gens}
 
     # # Dataset
 
@@ -53,140 +48,76 @@ def run_adult_experiments_trees_taxonomies(
 
     dfI = dfI[attributes + [target]]
 
-    saveFig = False
+
     import os
 
-    tree_outputdir = os.path.join(".", name_output_dir, "figures", "tree")
-
-    if saveFig:
-        from pathlib import Path
-
-        Path(tree_outputdir).mkdir(parents=True, exist_ok=True)
-
-    # # Tree divergence - income
+    # # Tree divergence - FPR
 
     all_time_results = {}
-    for type_experiment in type_experiments:
-        all_time_results[type_experiment] = {}
+    
+        
+    time_results = {"with_gen": {}, "without_gen": {}}
+
+
+    df_analyze = dfI.copy()
+
+    import time
+
+    start_time_tree = time.time()
+
+    from tree_discretization_ranking import TreeDiscretization_ranking
+
+    tree_discr = TreeDiscretization_ranking()
+
+    # ## Extract tree
+    generalization_dict, discretizations = tree_discr.get_tree_discretization(
+        df_analyze,
+        type_splitting=type_experiment,
+        min_support=min_support_tree,
+        metric=metric,
+        continuous_attributes=list(continuous_attributes),
+        storeTree=True,
+        type_criterion=type_criterion,
+        minimal_gain=minimal_gain,
+        target_col=target
+        # minimal_gain = 0.0015
+    )
+
+
+    time_results["tree_time"] = time.time() - start_time_tree
+
+    import json
+
+    with open(os.path.join(os.path.curdir, "datasets", "ACSPUMS", "adult_taxonomies.json"), "r") as fp:
+        generalization_dict_tax = json.load(fp)
+    
+
+    generalization_dict_all = deepcopy(generalization_dict)
+    generalization_dict_all.update(generalization_dict_tax)
+
+    for min_sup_divergence in min_sup_divergences:
         if verbose:
-            print(f"Experiment type: {type_experiment}")
-        for metric in metrics:
-            time_results = {"with_gen": {}, "without_gen": {}}
-            if verbose:
-                print(f"Metric: {metric}")
-                print(f"min_support_tree: {min_support_tree}")
-                print(f"type_criterion: {type_criterion}")
+            print(min_sup_divergence, end=" ")
 
-            outputdir = os.path.join(
-                ouput_folder_dir,
-                name_output_dir,
-                dataset_name,
-                type_criterion,
-                f"stree_{min_support_tree}",
-                metric,
-                type_experiment,
-                f"gain_{minimal_gain}",
+        # ## Extract divergence - 1 function
+
+        for apply_generalization in [False, True]:
+            if apply_generalization == False:
+                type_gen = "without_gen"
+            else:
+                type_gen = "with_gen"
+            from utils_extract_divergence_generalized_ranking import (
+                extract_divergence_generalized,
             )
 
-            from pathlib import Path
-
-            Path(outputdir).mkdir(parents=True, exist_ok=True)
-
-            df_analyze = dfI.copy()
-
-            import time
-
-            start_time_tree = time.time()
-
-            from tree_discretization_ranking import TreeDiscretization_ranking
-
-            tree_discr = TreeDiscretization_ranking()
-
-            # ## Extract tree
-            generalization_dict, discretizations = tree_discr.get_tree_discretization(
-                df_analyze,
-                type_splitting=type_experiment,
-                min_support=min_support_tree,
-                metric=metric,
-                continuous_attributes=list(continuous_attributes),
-                storeTree=True,
-                type_criterion=type_criterion,
-                minimal_gain=minimal_gain,
-                target_col=target
-                # minimal_gain = 0.0015
+            allow_overalp = (
+                True if type_experiment == "all_attributes" else False
             )
-
-            time_results["tree_time"] = time.time() - start_time_tree
-            n_internal_nodes, n_leaf_nodes = tree_discr.get_number_nodes()
-
-            with open(os.path.join(outputdir, "childs.json"), "w") as fp:
-                import json
-
-                json.dump(
-                    {
-                        "n_internal_nodes": n_internal_nodes,
-                        "n_leaf_nodes": n_leaf_nodes,
-                    },
-                    fp,
-                )
-
-            if show_fig:
-                from utils_print_tree import viz_tree
-
-                viz_tree(
-                    tree_discr,
-                    continuous_attributes=continuous_attributes,
-                    tree_outputdir=tree_outputdir,
-                    suffix=f"{type_experiment}_{type_criterion}_sd_{min_support_tree}_{metric}",
-                    saveFig=saveFig,
-                )
-
-            """
-            considerOnlyContinuos = True
-            if considerOnlyContinuos:
-                for k in list(generalization_dict.keys()):
-                    if k not in continuous_attributes:
-                        generalization_dict.pop(k, None)
-            """
-
-            import json
-
-            with open(os.path.join(os.path.curdir, "datasets", "ACSPUMS", "adult_taxonomies.json"), "r") as fp:
-                generalization_dict_tax = json.load(fp)
-            
-
-            generalization_dict_all = deepcopy(generalization_dict)
-            generalization_dict_all.update(generalization_dict_tax)
-
-            if no_compute_divergence:
+            if (allow_overalp) and (apply_generalization is False):
                 continue
 
-            for min_sup_divergence in min_sup_divergences:
-                if verbose:
-                    print(min_sup_divergence, end=" ")
-
-                # ## Extract divergence - 1 function
-
-                for apply_generalization in [False, True]:
-                    if apply_generalization == False:
-                        type_gen = "without_gen"
-                    else:
-                        type_gen = "with_gen"
-                    from utils_extract_divergence_generalized_ranking import (
-                        extract_divergence_generalized,
-                    )
-
-                    allow_overalp = (
-                        True if type_experiment == "all_attributes" else False
-                    )
-                    if (allow_overalp) and (apply_generalization is False):
-                        continue
-
-                    if verbose:
-                        print(f"({type_gen})", end=" ")
-                        print(f"({allow_overalp})", end=" ")
-                    start_time_divergence = time.time()
-                    FP_fm = extract_divergence_generalized(
+            start_time_divergence = time.time()
+            FP_fm = extract_divergence_generalized(
                         df_analyze,
                         discretizations,
                         generalization_dict,
@@ -195,87 +126,71 @@ def run_adult_experiments_trees_taxonomies(
                         apply_generalization=apply_generalization,
                         target_name=target,
                         FPM_type="fpgrowth",
-                        metrics_divergence=metrics,
+                        metrics_divergence=[metric],
                         allow_overalp=allow_overalp,
                         type_experiment=type_experiment,
                     )
-                    time_results[type_gen][min_sup_divergence] = (
-                        time.time() - start_time_divergence
-                    )
-                    
-
-                    from utils_experiments_runs import get_df_stats
-
-                    stats = get_df_stats(FP_fm, metrics=metrics)
-
-                    import json
-
-                    filename = os.path.join(
-                        outputdir,
-                        f"stats_sdiv{min_sup_divergence}_{type_gen}_{metric}.json",
-                    )
-
-                    with open(
-                        filename,
-                        "w",
-                    ) as fp:
-                        json.dump(stats, fp)
-
-                    out[metric]["FP"][type_experiment][type_gen][
-                        float(min_sup_divergence)
-                    ] = len(FP_fm)
-                    out[metric]["max"][type_experiment][type_gen][
-                        float(min_sup_divergence)
-                    ] = stats[metric]["max"]
-
-            if metric not in all_time_results:
-                all_time_results[metric] = {}
-            all_time_results[metric][type_experiment] = time_results
+            time_results[type_gen][min_sup_divergence] = (
+                time.time() - start_time_divergence
+            )
             if verbose:
-                print()
+                print(f"({round( time.time() - start_time_divergence,2)})", end = " ")
+            from utils_experiments_runs import get_df_stats
 
-    if no_compute_divergence:
-        return
+            from divexplorer_generalized.FP_Divergence import FP_Divergence
+            fp_i = FP_Divergence(FP_fm, metric)
+            FP_fm = fp_i.getDivergence(th_redundancy=0)
 
-    for metric in metrics:
-        outputdir = os.path.join(
-            ouput_folder_dir,
+            out["FP"][type_gen][
+                float(min_sup_divergence)
+            ] = FP_fm.shape[0]
+            out["max"][type_gen][
+                float(min_sup_divergence)
+            ] = max(FP_fm[metric])
+
+    all_time_results = time_results
+    if verbose:
+        print()
+
+    outputdir = os.path.join(
+        ouput_folder_dir,
             name_output_dir,
             dataset_name,
             type_criterion,
             f"stree_{min_support_tree}",
             metric,
-            f"gain_{minimal_gain}",
-        )
-        from pathlib import Path
+    )
+    from pathlib import Path
 
-        Path(outputdir).mkdir(parents=True, exist_ok=True)
+    Path(outputdir).mkdir(parents=True, exist_ok=True)
 
-        import json
+    import json
+
+    filename = os.path.join(
+        outputdir,
+        f"info_time.json",
+    )
+    with open(
+        filename,
+        "w",
+    ) as fp:
+        json.dump(all_time_results, fp)
+
+    for i in info_list:
+        output = out[i]
 
         filename = os.path.join(
             outputdir,
-            f"info_time.json",
+            f"info_ALL_{i}.json",
         )
+
         with open(
             filename,
             "w",
         ) as fp:
-            json.dump(all_time_results[metric], fp)
+            json.dump(output, fp)
 
-        for i in info_list:
-            output = out[metric][i]
 
-            filename = os.path.join(
-                outputdir,
-                f"info_ALL_{i}.json",
-            )
-
-            with open(
-                filename,
-                "w",
-            ) as fp:
-                json.dump(output, fp)
 
 
 import argparse
